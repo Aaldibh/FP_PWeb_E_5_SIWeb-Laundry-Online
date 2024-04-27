da7a7150ea2a0b831d6567e2017fece636a67a9a
Aaldibh <aldibagushermawan10@gmail.com>
2024-04-27 23:03:59 +0700

    updated 27 April'24 - 23.04

---
+++ FP_PWeb_5E/_login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Smart Laundry</title>
+        <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.css">
+        <script type="text/javascript" src="./assets/js/Jquery.js"></script>
+        <script type="text/javascript" src="./assets/js/bootstrap.js"></script>
+    </head>
+
+    <body style="background: white;">
+        <div class="container">
+            
+        </div>
+
+    </body>
+</html>
