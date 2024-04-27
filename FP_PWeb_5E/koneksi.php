7e3d2e5f1a0665be3698f000348114088332ebac
Aaldibh <aldibagushermawan10@gmail.com>
2024-04-27 22:47:15 +0700

    updated 27 April'24 - 22.47

---
+++ FP_PWeb_5E/koneksi.php
@@ -0,0 +1,7 @@
+<!-- koneksi laundyDB -->
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "laundryDB");
+if(mysqli_connect_errno()){
+    echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+?>
