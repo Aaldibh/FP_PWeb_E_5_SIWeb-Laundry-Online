faa189517d162a7103cacd5760b7fa7e0aba2ff9
Aaldibh <aldibagushermawan10@gmail.com>
2024-04-27 22:22:16 +0700

    updated 27 April'24 - 22.22

--- FP_PWeb_5E/_dashboard.php
+++ FP_PWeb_5E/_dashboard.php
@@ -12,11 +12,12 @@
         <header>
             <a href="#" class="logo">Smart Laundry</a>
             <ul class="nav">
-                <moveLink><a href="#">Beranda</a></moveLink>
-                <moveLink><a href="#">Layanan</a></moveLink>
-                <moveLink><a href="#">Login</a></moveLink>
-                <moveLink><a href="#">Testimoni</a></moveLink>
-                <moveLink><a href="#">Contact</a></moveLink>
+                <li><a href="#beranda">Beranda</a></li>
+                <li><a href="#about">Tentang Kami</a></li>
+                <li><a href="#layanan">Layanan</a></li>
+                <li><a href="#kontak">Kontak</a></li>
+                <li><a href="#" type="button" class="btnMasuk2">Masuk</a></li>
+                <li><a href="#" type="button" class="btnDaftar">Daftar</a></li>
             </ul>
         </header>
 
@@ -28,7 +29,7 @@
                     Buat kegiatan mencucimu menjadi<br>
                     lebih mudah dengan beberapa sentuhan.
                 </p>
-                <a href="#" class="btn-1">Masuk</a>
+                <a href="#" type="button" class="btnMasuk">Masuk</a>
             </div>
         </section>
 
@@ -39,7 +40,7 @@
                 <div class="row">
                     <div class="col50">
                         <div class="titleText">
-                            <span>A</span>bout Us
+                            <span>T</span>entang Kami
                         </div>
                         <p>Sistem informasi laundry berbasis web merupakan platform digital yang dirancang untuk mencapai SDGs nomor 9, 
                             yaitu meningkatkan infrastruktur yang tangguh, mempromosikan industrialisasi yang inklusif dan berkelanjutan, 
@@ -52,11 +53,6 @@
                             membantu pengusaha laundry dalam mengelola usahanya dengan lebih mudah dan efisien. Project ini diharapkan dapat 
                             menghasilkan sistem informasi laundry berbasis web yang bermanfaat bagi pengusaha laundry dan juga pelanggannya. </p>
                     </div>
-                    <!-- <div class="col50">
-                        <div class="imgBx">
-
-                        </div>
-                    </div> -->
                 </div>
             </div>
         </section>
@@ -65,7 +61,7 @@
         <section class="layanan" id="layanan">
             <div class="container">
                 <div class="title">
-                    <h2 class="titleText">Our <span>S</span>ervices</h2>
+                    <h2 class="titleText"><span>L</span>ayanan Kami</h2>
                     <p> Smart Laundri menyajikan beberapa layanan yang dapat membantu Anda untuk mencuci pakaian</p>
                 </div>
                 <div class="menu">
@@ -113,6 +109,58 @@
                 </div>
             </div>
         </section>
+
+        <!-- Kontak -->
+        <section class="kontak" id="kontak">
+            <div class="container">
+                <div class="title">
+                    <h2 class="titleText">Info <span>K</span>ontak</h2>
+                </div>
+
+                <div class="box">
+                    <div class="kontakForm">
+                        <h3>Kirim Pesan</h3>
+                        <div class="inputBox">
+                            <input type="text" placeholder="Name">
+                        </div>
+
+                        <div class="inputBox">
+                            <input type="text" placeholder="Email">
+                        </div>
+
+                        <div class="inputBox">
+                            <textarea type="text" placeholder="Kotak Pesan"></textarea>
+                        </div>
+                        
+                        <div class="inputBox">
+                            <input type="submit" class="btnSubmit" value="Kirim">
+                        </div>
+                    </div>
+                    
+                    <div class="map">
+
+                        <div class="info">
+                            <span>No.Telepon :</span>
+                            <p>+6281323xxxxxx</p>
+                        </div>
+
+                        <div class="info">
+                            <span>Email :</span>
+                            <p>smartlaundry@gmail.com</p>
+                        </div>
+
+                        <div class="info">
+                            <span>Alamat :</span>
+                            <p>Surabaya, Indonesia</p>
+                        </div><br>
+
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.1795614693174!2d112.78832469999999!3d-7.333721199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fab87edcad15%3A0xb26589947991eea1!2sUniversitas%20Pembangunan%20Nasional%20%22Veteran%22%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1714191004677!5m2!1sid!2sid"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Copyright -->
         
         <script src="LaundryAppJS.js"></script>
 </html>
