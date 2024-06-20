1aa0bb907d3d66c5f80986d8b2e0c49d040c2755
Teduhhh <daffateduh@gmail.com>
2024-06-20 07:45:58 +0700

    Update

--- FP_PWeb_5E/Owner/prosesEditLayanan.php
+++ FP_PWeb_5E/Owner/prosesEditLayanan.php
@@ -3,10 +3,9 @@ include_once("../koneksi.php");
 
 $idLayanan = $_POST['id'];
 $nama = $_POST['nama'];
-$jenispaket = $_POST['jenis'];
 $harga = $_POST['harga'];
 
-$query = "update tb_layanan set nama_layanan = '$nama', jenis_paket = '$jenispaket', harga_layanan = '$harga' where id_layanan = '$idLayanan'";
+$query = "update tb_layanan set nama_layanan = '$nama', harga_layanan = '$harga' where id_layanan = '$idLayanan'";
 $hasil = mysqli_query($koneksi, $query);
 
 if($hasil){
